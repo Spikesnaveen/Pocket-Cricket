4bfa12231b67e106924dfa2f00baf21c04469dd4
navee <navee@Naveen>
2022-08-13 14:45:27 +0530

    Initial commit

---
+++ src/main/java/com/pocket/cricket/PocketCricketApplication.java
@@ -0,0 +1,13 @@
+package com.pocket.cricket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocketCricketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocketCricketApplication.class, args);
+	}
+
+}
