ad08ae4fe21d7c61b070e83217e643a7aecff1f5
Spikesnaveen <naveensrinivasan984189@gmail.com>
2023-03-19 20:16:51 +0530

    added @SpringBootTest to test class

--- src/test/java/com/pocket/cricket/service/ServiceClassTest.java
+++ src/test/java/com/pocket/cricket/service/ServiceClassTest.java
@@ -4,12 +4,16 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import com.pocket.cricket.PocketCricketApplication;
 
-public class ServiceClassTest  extends PocketCricketApplication {
+@SpringBootTest
+public class ServiceClassTest {
 	
-	@InjectMocks
+	@Autowired
 	CacheExampleService cacheExampleService;
 	
 	@Test
